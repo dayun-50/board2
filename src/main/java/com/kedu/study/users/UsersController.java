af2915d6836ed9ae6444dbfbef77453628b235f3
heybin <tlszn10@naver.com>
2025-09-29 15:15:00 +0900

    살려주세요토할거같아요

--- src/main/java/com/kedu/study/users/UsersController.java
+++ src/main/java/com/kedu/study/users/UsersController.java
@@ -1,7 +1,11 @@
 package com.kedu.study.users;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,21 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/users")
 @RestController
+@CrossOrigin(origins = "*")
 public class UsersController {
 	@Autowired
 	private UsersService uServ;
 	
 	// 회원가입
 	@PostMapping
-	public ResponseEntity<Void> userInsert(@RequestBody UsersDTO dto){
-		uServ.insert(dto);
-		return ResponseEntity.ok().build();
+	public ResponseEntity<Map<String, Object>> userInsert(@RequestBody UsersDTO dto){
+	    Map<String, Object> result = new HashMap<>();
+	    try {
+	        uServ.insert(dto);
+	        result.put("success", true);
+	        result.put("message", "회원가입 완료!");
+	    } catch(Exception e) {
+	        result.put("success", false);
+	        result.put("message", e.getMessage());
+	    }
+	    return ResponseEntity.ok(result);
 	}
 	
 	// 로그인
 	@GetMapping
 	public ResponseEntity<String> login(String id, String pw){
-		String name = uServ.login(id, pw);
-		return ResponseEntity.ok(name);
+		uServ.login(id, pw);
+		return ResponseEntity.ok().build();
 	}
 }
