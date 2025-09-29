7625f7f4ac864c111c57547655ca8fb8d7dbdddf
heybin <tlszn10@naver.com>
2025-09-29 10:51:08 +0900

    서버래요

---
+++ src/main/java/com/kedu/study/Study01BoradApplication.java
@@ -0,0 +1,13 @@
+package com.kedu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study01BoradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study01BoradApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kedu/study/Study01BoradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedu.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study01BoradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
